f124fd720306ec91061c536c711b1754258c7c85
takeshi <111test33333354@mail.com>
2020-10-26 13:40:02 +0900

    デモページ修正

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # amp-bootstrap5
 amp portfolio site with bootstrap5
+
+## DEMO page
+[demo](https://takeshi-du.github.io/amp-bootstrap5/)
--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ sass.compiler = require('node-sass');
 
 function sassCompile(cb) {
   // 複数のファイルを指定する場合は配列使用可能
-  return src(['./src/_sass/style.scss'])
+  return src(['./_sass/style.scss'])
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(dest('./src/_includes/css'));
+    .pipe(dest('./_includes/css'));
 }
 
 exports.default = sassCompile
